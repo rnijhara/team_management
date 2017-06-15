4521d9f4e64a426ca03111012caf8d97743589b4
Roopak Nijhara <roopak@airloyal.com>
2017-06-15 19:44:58 +0530

    Using generic class based views.

--- teamapp/views.py
+++ teamapp/views.py
@@ -4,49 +4,18 @@ from __future__ import unicode_literals
 from teamapp.models import TeamMember
 from teamapp.serializers import TeamMemberSerializer
 
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics
+
+# Using generic class-based views
 
 
-class TeamMemberDetail(APIView):
+class TeamMemberDetail(generics.RetrieveUpdateDestroyAPIView):
 
-    def get_object(self, pk):
-        try:
-            return TeamMember.objects.get(pk=pk)
-        except TeamMember.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        team_member = self.get_object(pk)
-        serializer = TeamMemberSerializer(team_member)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        team_member = self.get_object(pk)
-        serializer = TeamMemberSerializer(team_member, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        team_member = self.get_object(pk)
-        team_member.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    queryset = TeamMember.objects.all()
+    serializer_class = TeamMemberSerializer
 
 
-class TeamMemberList(APIView):
+class TeamMemberList(generics.ListCreateAPIView):
 
-    def get(self, request, format=None):
-        team_members = TeamMember.objects.all()
-        serializer = TeamMemberSerializer(team_members, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = TeamMemberSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = TeamMember.objects.all()
+    serializer_class = TeamMemberSerializer
